62d8a9d5753908fa795c61ba88fecbf6e0fb9f10
amanRecreates <amanofficial950@gmail.com>
2021-07-28 01:44:17 +0530

    with sign

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
     const [counter, setCounter] = useState(0)
     const [ isTyped, setIsTyped] = useState(false)
     const [listSpan, setListSpan] = useState(null)
-    let para = 'what are you doing OKAY'
+    let para = 'what are you'
     
     useEffect(() => {
         setTimeout(() => {
