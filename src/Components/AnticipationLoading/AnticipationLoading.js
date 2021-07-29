30901cab11c2921adf4df7a962ca3b7fd7837d67
amanRecreates <amanofficial950@gmail.com>
2021-07-29 15:32:19 +0530

    replaced dummy text, with actual loader

--- src/Components/AnticipationLoading/AnticipationLoading.js
+++ src/Components/AnticipationLoading/AnticipationLoading.js
@@ -1,10 +1,23 @@
 import './AnticipationLoading.scss'
+import { useState, useEffect } from 'react'
 
 const AnticipationLoading = () => {
+    const [time, setTime] = useState(3)
+
+    useEffect(() => {
+        let interval = setInterval(() => {
+            setTime(time => time - 1)
+        }, 1000)
+
+        return () => {
+            // cleanup function
+            clearInterval(interval)
+        }
+    }, [])
 
     return (
-        <h1>This is Anticipation Loader...</h1>
+        <h1>{time}</h1>
     );
 }
- 
+
 export default AnticipationLoading;
