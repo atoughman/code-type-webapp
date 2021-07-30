b92779b80382280a89f2cbea1da01ef46dbee078
amanRecreates <amanofficial950@gmail.com>
2021-07-30 14:00:19 +0530

    [ DOCUMENTATION ] added documentation

--- src/Components/CustomText/CustomText.js
+++ src/Components/CustomText/CustomText.js
@@ -1,5 +1,12 @@
 import './CustomText.scss'
 
+/**
+ * It provides the text which is to be uploaded as givenText. so user can practice his own text.
+ * 
+ * @param {string} cusText Text provided by user, to be uploaded as his/her practice (givenText)
+ * @param {function} setCusText It is useState method 
+ * @returns This component displays an input field in UI
+ */
 const CustomText = ({cusText, setCusText}) => {
     return (
         <input type="text" value={cusText} onChange={(e) => setCusText(e.target.value)}/>
