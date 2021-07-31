cd74c473ba6d0e91c5925413c94e8cfcd128a0bb
amanRecreates <amanofficial950@gmail.com>
2021-07-31 10:20:32 +0530

    added text file reading support

--- src/Components/CustomText/CustomText.js
+++ src/Components/CustomText/CustomText.js
@@ -5,12 +5,28 @@ import './CustomText.scss'
  * 
  * @param {string} cusText Text provided by user, to be uploaded as his/her practice (givenText)
  * @param {function} setCusText It is useState method 
- * @returns This component displays an input field in UI
+ * @returns This component displays an input field and uploadFile button in UI
  */
-const CustomText = ({cusText, setCusText}) => {
+const CustomText = ({ cusText, setCusText }) => {
+
+    let showFile = async (e) => {
+        // e.preventDefault()
+        const reader = new FileReader()
+        reader.readAsText(e.target.files[0])  // reads and stores value in e.target.result
+        reader.onload = async (e) => {
+            const text = (e.target.result)
+            setCusText(text)
+        };
+    }
+
     return (
-        <input type="text" value={cusText} onChange={(e) => setCusText(e.target.value)}/>
+        <>
+            <input type="text" value={cusText} onChange={(e) => setCusText(e.target.value)} />
+            <div>
+                <input type="file" onChange={(e) => showFile(e)} />
+            </div>
+        </>
     );
 }
- 
+
 export default CustomText;
