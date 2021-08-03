e0cb933bc4f4c1b63d6ed0825b08bd4336c95b0f
amanRecreates <amanofficial950@gmail.com>
2021-08-03 23:44:46 +0530

    added typewriter animation as loader

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,6 +2,9 @@ import { useHistory } from "react-router-dom";
 import './Home.scss'
 
 import video from '../../assets/typing.mp4'
+import typewriter from '../../assets/typewriter.mp4'
+
+import { useEffect, useState } from "react";
 
 const Home = () => {
 
@@ -11,9 +14,18 @@ const Home = () => {
         history.push('/practice')
     }
 
+    useEffect(() => {
+        setTimeout(() => {
+            document.querySelector('.loader').classList.add('hide')
+        }, 2000);
+    }, [])
+
     return (
         <div className="home">
-            <video autoplay="true" loop="true" muted="true" src={video} />
+            <div className="loader">
+                <video className="typewriter" autoplay="true" loop="true" muted="true" src={typewriter} />
+            </div>
+            <video className="type" autoplay="true" loop="true" muted="true" src={video} />
             <div className="main">
                 <h1>Increase Your Coding Speed</h1>
                 <button onClick={handleOnClick}>Start Practice</button>
